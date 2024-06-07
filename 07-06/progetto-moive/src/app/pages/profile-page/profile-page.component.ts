0ef7f0e1cdcadcd733a134e80d578e7a4514a74b
Simone Diamantini <diama33@gmail.com>
2024-06-07 17:01:15 +0200

    07-06

--- 07-06/progetto-moive/src/app/pages/profile-page/profile-page.component.ts
+++ 07-06/progetto-moive/src/app/pages/profile-page/profile-page.component.ts
@@ -1,23 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { SignUp } from '../../interfaces/auth.interface';
 import { AuthService } from '../../services/auth.service';
-
+import { IMovie } from '../../interfaces/movie.interface';
+import { FilmsService } from '../../services/films.service';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.scss']
+  styleUrls: ['./profile-page.component.scss'],
 })
 export class ProfilePageComponent implements OnInit {
-  user:SignUp = {} as SignUp
-  constructor(private authSrv: AuthService) { }
+  movies: IMovie[] = [];
+
+  constructor(private filmsService: FilmsService) {}
 
   ngOnInit(): void {
-    this.authSrv.currentLoggedUsed()?.subscribe(user => this.user = user[0])
+    this.loadAllMovies();
   }
 
-  logout() {
-    this.authSrv.logout()
+  loadAllMovies(): void {
+    this.filmsService.getAllMovies().subscribe(
+      (movies: IMovie[]) => {
+        this.movies = movies;
+      },
+      (error) => {
+        console.error('Error loading movies:', error);
+      }
+    );
   }
-
 }
--- 07-06/progetto-moive/src/app/services/films.service.ts
+++ 07-06/progetto-moive/src/app/services/films.service.ts
@@ -1,11 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IMovie } from '../interfaces/movie.interface';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FilmsService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-
+  getAllMovies(): Observable<IMovie[]> {
+    return this.http.get<IMovie[]>(`${environment.srvr}movies`);
+  }
 }
