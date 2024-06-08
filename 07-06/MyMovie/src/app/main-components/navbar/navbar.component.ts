7a44759ba353239ae0e3cdf35567c5d38efed4a6
Simone Diamantini <diama33@gmail.com>
2024-06-08 13:36:03 +0200

    08-06

--- 07-06/MyMovie/src/app/main-components/navbar/navbar.component.ts
+++ 07-06/MyMovie/src/app/main-components/navbar/navbar.component.ts
@@ -1,20 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
+import { iUsers } from '../../interfaces/iUsers.interface';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
+  user: iUsers | null = null;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authService.isLoggedIn$.subscribe(
-      (isLoggedIn) => (this.isLoggedIn = isLoggedIn)
-    );
+    this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
+      this.isLoggedIn = isLoggedIn;
+      if (isLoggedIn) {
+        this.authService.user$.subscribe((user) => (this.user = user));
+      } else {
+        this.user = null;
+      }
+    });
   }
   logout() {
     this.authService.logout();
