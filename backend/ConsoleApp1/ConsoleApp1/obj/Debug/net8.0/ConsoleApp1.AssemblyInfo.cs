35e520aad01bb6f2cbde5d0cddea1c76922f2e7d
Simone Diamantini <diama33@gmail.com>
2024-06-18 15:57:47 +0200

    18/06

--- backend/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ backend/ConsoleApp1/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a44759ba353239ae0e3cdf35567c5d38efed4a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6f411e1ead65f4efc3d92955cf34fca78ef9504")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
