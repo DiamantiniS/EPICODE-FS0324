d1ce56eb4eade51db06cd74419f71c001a2ada5a
Simone Diamantini <diama33@gmail.com>
2024-06-27 17:24:24 +0200

    27-06

--- backend/CinemaTicketSystem/Identiti/Cinema.cs
+++ backend/CinemaTicketSystem/Identiti/Cinema.cs
@@ -5,7 +5,7 @@ namespace CinemaTicketSystem.Identiti
     public class Cinema
     {
         public string Nome { get; set; }
-        public int CapacitaMassima { get; set; } = 120;
+        public int CapacitaMassima { get; set; } = 2;
         public int BigliettiVenduti { get; set; }
         public int BigliettiRidottiVenduti { get; set; }
 
