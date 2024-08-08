ac1e8cffd5f2ce4954d7cd6b75d38ffc685c5e60
Simone Diamantini <diama33@gmail.com>
2024-08-08 23:04:54 +0200

    senza css

---
+++ backend/BW5/BW5/Program.cs
@@ -0,0 +1,54 @@
+using ApplicationDbContex.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Aggiungi servizi al contenitore.
+builder.Services.AddControllersWithViews();
+
+// Configura il DbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configura l'autenticazione
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login/Login";
+        options.LogoutPath = "/Login/Logout";
+    });
+
+// Configura le policy di autorizzazione
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("VeterinarioPolicy", policy => policy.RequireRole("Veterinario"));
+    options.AddPolicy("FarmacistaPolicy", policy => policy.RequireRole("Farmacista"));
+});
+
+// Configura il servizio di gestione delle autenticazioni
+//builder.Services.AddScoped<IAuthService, AuthService>();
+
+var app = builder.Build();
+
+// Configura la pipeline HTTP.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
